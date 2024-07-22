5144dd0072e28950f4bb03ae9f90b3c63b48187f
Happy <happyseoketsa@gmail.com>
2024-07-22 15:17:28 +0200

    updated

--- App/models.py
+++ App/models.py
@@ -1,13 +1,14 @@
 # This file handles database operations
 
 from flask_mysqldb import MySQL
+import os
 
 def kasithreads_db(app):
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'KasiThreads@31'
-    app.config['MYSQL_DB'] = 'kasithreads_db'
+    # Use environment variables for sensitive information
+    app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
+    app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
+    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'password')
+    app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'database_name')
 
     mysql = MySQL(app)
     return mysql
-
